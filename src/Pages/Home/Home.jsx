845adfb58c8594fe1bf35d36fc20ae32b4847dec
RakibHossen678 <rh491464@gmail.com>
2024-09-09 14:14:09 +0600

    added category section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./Banner";
 import Categories from "./Categories";
+import HeroSection from "./HeroSection";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <Banner/>
       <Categories/>
+      <HeroSection/>
     </>
   );
 };
