4068b0e87642405ce9d00d5a8b4289cd464f9fc0
RakibHossen678 <rh491464@gmail.com>
2024-09-09 13:59:43 +0600

    added category section

---
+++ src/Pages/Home/Categories.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import cat1 from "../../assets/category-1.jpg";
+import cat2 from "../../assets/category-2.jpg";
+import cat3 from "../../assets/category-3.jpg";
+import cat4 from "../../assets/category-4.jpg";
+
+const Categories = () => {
+  const categories = [
+    { name: "Accessories", path: "accessories", image: cat1 },
+    { name: "Dress Collection", path: "dress", image: cat2 },
+    { name: "Jewelry", path: "jewelry", image: cat3 },
+    { name: "Cosmetics", path: "cosmetics", image: cat4 },
+  ];
+  return (
+    <>
+      <div className="product__grid">
+        {categories.map((category, idx) => (
+          <Link
+            key={idx}
+            to={`categories/${category.path}`}
+            className="categories__card"
+          >
+            <img src={category.image} alt={category.name} />
+            <h4>{category.name} </h4>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
