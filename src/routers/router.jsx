41951b3700b6e4065c51eebae700d0a887e4333a
RakibHossen678 <rh491464@gmail.com>
2024-09-09 13:23:08 +0600

    added banner section

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../Pages/Home/Home";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -7,7 +8,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Home page</h1>,
+        element: <Home />,
       },
       {
         path: "/about",
