40e7e3e6dbf6fdb2f446d1318ba576768509ab56
RakibHossen678 <rh491464@gmail.com>
2024-09-16 16:23:49 +0600

    added responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,58 @@ import CartModal from "../Pages/shop/CartModal";
 
 const Navbar = () => {
   const products = useSelector((state) => state.cart.products);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isCartOpen, setIsCarOpen] = useState(false);
   const handleCartToggle = () => {
     setIsCarOpen(!isCartOpen);
   };
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <header className=" ">
+      {isMenuOpen && (
+        <div
+          onClick={closeMenu}
+          className={`fixed z-[1000] inset-0 bg-black bg-opacity-80 transition-opacity duration-300 ${
+            isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className={`fixed z-[1001] inset-y-0 left-0 bg-white w-2/3 max-w-xs h-full shadow-lg transform transition-transform duration-300 ease-in-out ${
+              isMenuOpen ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            <div className="nav__logo pt-8 px-10">
+              <Link to={"/"}>
+                StyleBask <span>.</span>
+              </Link>
+            </div>
+            <ul className=" pt-10 px-10 space-y-7">
+              <li className="link">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="link">
+                <Link to="/shop">Shop</Link>
+              </li>
+              <li className="link">
+                <Link to="/pages">Pages</Link>
+              </li>
+              <li className="link">
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
       <nav>
+        <button onClick={handleMenuToggle} className=" lg:hidden block">
+          <i className="ri-menu-2-line text-2xl pr-2"></i>
+        </button>
         <ul className="nav__links">
           <li className="link">
             <Link to="/">Home</Link>
